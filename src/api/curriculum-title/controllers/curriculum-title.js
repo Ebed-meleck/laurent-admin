6800ef601473ce193ef187d627ceb6b5cefde886
Ebed-meleck <ebedmeleckmakoso@gmail.com>
2023-09-20 23:44:21 +0100

    v1 api

---
+++ src/api/curriculum-title/controllers/curriculum-title.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * curriculum-title controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::curriculum-title.curriculum-title');
---
+++ src/api/curriculum-title/services/curriculum-title.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * curriculum-title service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::curriculum-title.curriculum-title');
